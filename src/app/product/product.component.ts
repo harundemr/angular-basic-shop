e1444ed22af00f06703ba70c80ab56b7f25bc932
Harun Demir <harundemirp@gmail.com>
2021-01-15 23:08:16 +0300

    global servis oluşturma

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
-declare let alertify: any;
+import { AlertifyService } from '../services/alertify.service'
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -8,7 +8,7 @@ declare let alertify: any;
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertifyService: AlertifyService) { }
   title = "Ürün Listesi"
   filterText = ""
   products: Product[] = [
@@ -46,8 +46,8 @@ export class ProductComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  addToCart(product:Product){
-    alertify.success(product.name + " sepete eklendi!");
+  addToCart(product: Product) {
+    this.alertifyService.success(product.name + " sepete eklendi!");
   }
 
 }
