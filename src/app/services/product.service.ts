b4dacb3e0e485c8d30c8a391e1268866cef4deed
Harun Demir <harundemirp@gmail.com>
2021-01-15 23:54:59 +0300

    rxjs ve observeble implementasyonu yapıldı

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Product } from '../product/product';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 @Injectable()
 export class ProductService {
 
   constructor(private http: HttpClient) { }
   path = "http://localhost:3000/products";
 
-  getProduct():Observable<Product[]>{
-    return this.http.get<Product[]>(this.path);
+  getProduct(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.path).pipe(
+      tap(data => console.log(JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(err: HttpErrorResponse) {
+    if (err.error instanceof ErrorEvent) {
+      return throwError('Bir hata oluştu ' + err.error.message);
+    } else {
+      return throwError("Sistemsel bir hata")
+    }
   }
 }
